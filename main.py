c2536fc42838a5e66677b3d311efc966f008ebf4
SkyYonG <skyyong078@gmail.com>
2019-10-23 16:45:18 +0900

    feat: change i -> a
    I hate alphabet "i", so I changed "i" to "a".

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-for i in range(1,100+1):
+for a in range(1,100+1):
 
-
-  if i%15==0: 
+  if a%15==0: 
     print("fizzbuzz")
 
-  elif i%3==0 :
+  elif a%3==0 :
     print("fizz")
-  elif i%5==0 :
+  elif a%5==0 :
     print("buzz")
   else:
-    print(i)
-
+    print(a)
 
