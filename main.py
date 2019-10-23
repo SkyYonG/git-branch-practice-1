c758691a9498bc6f479603c8c2dc2211266d0d73
dayoungMM <dalbeetm@gmail.com>
2019-10-23 15:45:31 +0900

    feat: if-add

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("python works!")
+i = int(input("type some number 1 to 100: "))
+
+if i%15==0: 
+    print("fizzbuzz")
+
+elif i%3==0 :
+    print("buzz")
+elif i%5==0 :
+    print("fizzbuzz")
+else:
+    print(i)
+
